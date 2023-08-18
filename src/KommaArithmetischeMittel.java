bf6cbef5aa66fed4e182d289bc0d1a6d55c71948
domejek <dominik.jeksties@icloud.com>
2023-08-18 07:58:26 +0200

    18.08 bis Nr. 10

--- src/KommaArithmetischeMittel.java
+++ src/KommaArithmetischeMittel.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class KommaArithmetischeMittel {
     public static void main (String[]argv){
-        //
+        int anzahl;
+        double summe = 0.0;
+        System.out.println("Anzahl:");
+        Scanner sc = new Scanner(System.in);
+        anzahl = sc.nextInt();
+        for (int i = 1; i <= anzahl; i++) {
+            System.out.print("Zahl " + i + ": ");
+            summe += sc.nextDouble();
+        }
+        double mittel = summe / anzahl;
+        System.out.println("Das arithmetische Mittel ist: " + mittel);
+        sc.close();
     }
 }
---
+++ src/UmgekehrteZahlen.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class UmgekehrteZahlen {
+    public static void main (String[]argv){
+
+    }
+}
