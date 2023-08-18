998612f8eb75a7b5f75944378f56ae9e52a6f715
domejek <dominik.jeksties@icloud.com>
2023-08-18 08:25:28 +0200

    18.08 bis Nr. 10

--- src/UmgekehrteZahlen.java
+++ src/UmgekehrteZahlen.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class UmgekehrteZahlen {
     public static void main (String[]argv){
-
+        double zahl;
+        System.out.println("Zahl:");
+        Scanner sc = new Scanner(System.in);
+        zahl = sc.nextDouble();
     }
 }
